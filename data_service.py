002aaf676f92536895e73a973859487dda5f5366
nbaltodano <nicholasbaltodano@gmail.com>
2022-08-18 00:06:31 -0500

    Access API

--- data_service.py
+++ data_service.py
@@ -1,3 +1,13 @@
+import cryptowatch as cw
 #TODO Access API to get data
 #TODO Create database tables to store data
 #TODO Store Data In tables
+#TODO run the service every minute
+
+
+
+def get_price(code: str):
+    response = cw.markets.get(code)
+    return response.market.price.last
+
+print(get_price("KRAKEN:BTCUSD"))
