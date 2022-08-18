2b2e7cf3b719deb886f0fd3a57ef4c53758a778d
nbaltodano <nicholasbaltodano@gmail.com>
2022-08-18 00:15:30 -0500

    defined PricePoint table

---
+++ database.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.sqlite import *
+from sqlalchemy.orm import sessionmaker, relationship
+import uuid
+
+#TODO Make this configurable through conf file/command line arg
+info = 'sqlite:///montecarlo.db'
+
+Base = declarative_base()
+
+#TODO 
+class PricePoint(Base):
+    """Define the Price Point table. 
+        Columns:
+            id          -TEXT
+            code        -TEXT
+            exchange    -TEXT
+            market      -TEXT
+            price       -TEXT
+            time        -DATETIME(sqlite text)"""
+    __tablename__ = "PricePoint"
+    
+    id       = Column('id', TEXT,  primary_key=True)
+    code     = Column('code', TEXT)   
+    exchange = Column('exchange', TEXT)
+    market   = Column('market', TEXT)
+    price    = Column('price', TEXT)
+    time     = Column('time', DATETIME)
+
+    def __init__(self, code, exchange, price, time, market):
+        self.id = str(uuid.uuid1())
+        self.exchange = exchange
+        self.price = price
+        self.time = time
+        self.code = code
+        self.market = market
+    
+    def __repr__(self):
+        return f"PricePoint({self.id}, {self.exchange}, {self.price}, {self.time}, {self.code})"
